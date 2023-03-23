212962815e0e050e11bd71243f33998ecad42cb5
Thales Morais <thalesmn2017@gmail.com>
2023-03-22 22:33:28 -0300

    Update Recebedor.java

--- Recebedor.java
+++ Recebedor.java
@@ -15,7 +15,8 @@ public class Recebedor implements Runnable {
     // recebe msgs do servidor e imprime na tela
     Scanner s = new Scanner(this.servidor);
     while (s.hasNextLine()) {
-        }       
+      //System.out.println(s.nextLine());
+        }  
      }
    }
  
